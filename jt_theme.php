2cf13605579886d486d8aa986a7eda7e40ebcf13
CaMnO <nyashkovd@gmail.com>
2025-09-02 16:51:51 +0500

    cosmy_seo/

--- jt_theme.php
+++ jt_theme.php
@@ -11,17 +11,17 @@ add_filter('pre_set_site_transient_update_plugins', function($transient) {
     $plugin_file = plugin_basename(__DIR__ . '/index.php');
     $plugin_data = get_plugin_data(__DIR__ . '/index.php');
     $current_version = $plugin_data['Version'];
-
+    $plug_refresh_check = "https://raw.githubusercontent.com/TheSmileGod/Cosmy_seo/main/update/cosmy-plugin-update.json";
     $plug_url = 'https://github.com/TheSmileGod/Cosmy_seo';
-    $plug_refresh_check = $plug_url.'/update/cosmy-plugin-update.json';
+    
     $remote = wp_remote_get($plug_refresh_check);
     if (is_wp_error($remote) || 200 !== wp_remote_retrieve_response_code($remote)) return $transient;
     $remote = json_decode(wp_remote_retrieve_body($remote));
-    $plugin = 'autowp-cosmy-api/index.php';
+    $plugin = 'cosmy_seo/index.php';
 
     if ($remote && version_compare($current_version, $remote->version, '<')) {
         $res = new stdClass();
-        $res->slug = 'autowp_cosmy_api';
+        $res->slug = 'cosmy-seo';
         $res->plugin = $plugin;
         $res->new_version = $remote->version;
         $res->package = $remote->download_url;
