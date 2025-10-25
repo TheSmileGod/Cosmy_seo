58fd01fd58d3cb59fba55b6c4e591e31c4cfcc23
CaMnO <nyashkovd@gmail.com>
2025-10-25 11:10:01 +0500

    227

--- cosmy_seo/index.php
+++ cosmy_seo/index.php
@@ -4,7 +4,7 @@
 Plugin Name: COSMY SEO
 Text Domain: cosmy-seo
 Description: COSMY Site — SEO module via REST API.
-Version: 2.2.6
+Version: 2.2.7
 Author: COSMY Site 
 Author URI: https://cosmy.site
 Update URI: https://github.com/TheSmileGod/Cosmy_seo
